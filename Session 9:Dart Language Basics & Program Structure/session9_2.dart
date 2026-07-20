93019fe13aa60040fba2f9922c54673eaaecca6b
Hetvi Virdiya <virdiyahetvi@gmail.com>
2026-07-20 11:43:47 +0530

    Add product variables and print message in Dart
    Declare variables for a Flipkart product and print details using string interpolation.

--- Session 9:Dart Language Basics & Program Structure/session9_2.dart
+++ Session 9:Dart Language Basics & Program Structure/session9_2.dart
@@ -1,3 +1,8 @@
+2.
+Write Dart code in main.dart to declare variables for a Flipkart product: productName (String),
+productPrice (double), and isInStock (bool). Print a message like 'Product: iPhone 13, Price: 69999.0, In Stock: true' using string interpolation.
+
+
 void main()
 {
   String productName="iPhone 13";
