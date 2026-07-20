a4e0515de8b34945989cf863509082de59a47330
Hetvi Virdiya <virdiyahetvi@gmail.com>
2026-07-20 11:40:25 +0530

    Create session9_1.dart

---
+++ Session 9:Dart Language Basics & Program Structure/session9_1.dart
@@ -0,0 +1,11 @@
+  void main()
+{
+  String foodname="pizza";
+  double price=99.10;
+  bool isvegetarian=false;
+
+  print("your favorite food is $foodname");
+  print("price is $price");
+  print("vegetarian $isvegetarian");
+
+}
