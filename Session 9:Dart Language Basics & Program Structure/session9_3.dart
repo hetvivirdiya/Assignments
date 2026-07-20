cba772f9fec460d292d24c2d8107aec9cd893660
Hetvi Virdiya <virdiyahetvi@gmail.com>
2026-07-20 11:44:18 +0530

    Implement follower and following difference calculation
    Added code to calculate and print the difference between followers and following.

--- Session 9:Dart Language Basics & Program Structure/session9_3.dart
+++ Session 9:Dart Language Basics & Program Structure/session9_3.dart
@@ -1,3 +1,8 @@
+3.
+Declare two integer variables in main.dart:
+followers and following for a user profile (like Instagram). Calculate and print the difference (followers - following) to the console.
+
+
 void main()
 {
   var follower=1500;
