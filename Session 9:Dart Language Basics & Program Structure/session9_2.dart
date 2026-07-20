f7e20e9405a93d9fc6d96cd0e8ec566fbb6dbb11
Hetvi Virdiya <virdiyahetvi@gmail.com>
2026-07-20 11:41:16 +0530

    Add basic Dart program for product details

---
+++ Session 9:Dart Language Basics & Program Structure/session9_2.dart
@@ -0,0 +1,8 @@
+void main()
+{
+  String productName="iPhone 13";
+  double productPrice=69999.0;
+  bool isInstock=true;
+
+  print("Product: $productName , Price:$productPrice, IisInstock:$isInstock");
+}
