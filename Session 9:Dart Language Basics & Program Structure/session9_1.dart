b062038b50579f607231d2e8ca19be8a89ffeb87
Hetvi Virdiya <virdiyahetvi@gmail.com>
2026-07-20 11:43:13 +0530

    Update session9_1.dart with variable declaration instructions
    Added instructions for declaring food variables and printing them.

--- Session 9:Dart Language Basics & Program Structure/session9_1.dart
+++ Session 9:Dart Language Basics & Program Structure/session9_1.dart
@@ -1,4 +1,10 @@
-  void main()
+1.
+Open main.dart in your Flutter project, declare variables for 
+your favorite food's name (String), its price (double), and whether it's vegetarian (bool),
+then print all three values to the console using print().
+
+  
+void main()
 {
   String foodname="pizza";
   double price=99.10;
