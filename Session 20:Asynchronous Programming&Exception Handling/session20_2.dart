580324d1c8f83ab70c8e0c0997663b22785520a3
Hetvi Virdiya <virdiyahetvi@gmail.com>
2026-09-10 22:21:45 +0530

    Create session20_2.dart

---
+++ Session 20:Asynchronous Programming&Exception Handling/session20_2.dart
@@ -0,0 +1,15 @@
+2.
+Build a simple Dart function getOrderStatus() that simulates checking a Zomato order status using Future.delayed for 3 seconds, then returns 'Order Delivered'.
+Call this function using async/await and print the result.
+
+
+  Future getOrderStatus()
+{
+  return Future.delayed(Duration(seconds: 3),()=>'Order Delivered',
+  );
+}
+void main()async
+{
+  var status=await getOrderStatus();
+  print(status);
+}
