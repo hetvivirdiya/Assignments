5be78e31eda53a61e9b15ed25a06ef793d372f7e
Hetvi Virdiya <virdiyahetvi@gmail.com>
2026-09-10 22:17:05 +0530

    Create session16_1.dart

---
+++ Session 16:Working with Maps&Key-Value data/session16_1.dart
@@ -0,0 +1,13 @@
+1.
+Create a Dart Map called foodItem with keys 'name', 'price', and 'category' to represent a Zomato menu item, and print each value using its key.
+
+
+  void  main()
+{
+  Map foodItem={
+    'name':'pizza',
+    'price':199,
+    'category':'Italian'
+  };
+  print(foodItem);
+}
